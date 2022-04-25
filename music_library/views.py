df9bef1d7e296b4509974b826c882565d897696e
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-04-25 09:10:34 -0700

    Created views in views file + adjusted url in url file

--- music_library/views.py
+++ music_library/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SongSerializer
+from .models import Song
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def music_library_list(request):
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def music_library_detail(request, pk):
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == 'GET':
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'DELETE':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        song.delete()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
--- music_library_project/urls.py
+++ music_library_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/music_library/', include('music_library.urls'))
+    path('music_library/', include('music_library.urls'))
 ]
