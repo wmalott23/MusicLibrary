2debfcbb7c195fa70be131bc3980e7012170a33e
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-04-25 08:46:30 -0700

    Created song model in model.py

--- music_library/models.py
+++ music_library/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    title = models.CharField(max_length=40)
+    artist = models.CharField(max_length=40)
+    album = models.CharField(max_length=40)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=40)
